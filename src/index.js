1e4d135845b6270fc9f91d57a7385ef1e344472d
Daniel Stephens <daniel.stephens@fairfaxmedia.com.au>
2023-06-07 00:16:40 +0000

    Merged in dev/CTV-3848-clean-up-json (pull request #7)
    [CTV-3848]: simplify json and use react 18 createRoot
    
    * [CTV-3848]: simplify json and use react 18 createRoot
    
    
    Approved-by: Jerome Salimao
    Approved-by: Tae-gyu Yun

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import "./styles.css";
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+const root = createRoot(mountNode);
+root.render(<App />);
